bbcbae651de2d7e798e249ea1e7080937c0c00b1
HyDrAa06 <108666340+HyDrAa06@users.noreply.github.com>
2022-07-04 14:03:24 +0300

    Add files via upload

---
+++ Дискриминанта на квадратно уравнение/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Дискриминанта_на_квадратно_уравнение
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, discriminant;
+            Console.Write("a= ");
+            a=double.Parse(Console.ReadLine());
+
+            Console.Write("b= ");
+            b = double.Parse(Console.ReadLine());
+
+            Console.Write("c= ");
+            c = double.Parse(Console.ReadLine());
+
+            discriminant = b * b -4 * a * c;
+
+            Console.Write("Discriminant= {0}",discriminant);
+            
+
+            Console.ReadLine();
+        }
+    }
+}
